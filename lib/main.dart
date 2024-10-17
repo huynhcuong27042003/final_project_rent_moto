b4e3164a16a37d94eb7d57f6e1102f1567f6859c
huynhcuong27042003 <huynhminhcuong.270403@gmail.com>
2024-10-17 16:46:13 +0700

    connect firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rent_moto/screens/auth/login/login_screen.dart';
-import 'package:flutter_rent_moto/screens/auth/signup/signup_enter_code_screen.dart';
-import 'package:flutter_rent_moto/screens/auth/signup/signup_enter_password_screen.dart';
 
 void main() async {
   runApp(const MyApp());
